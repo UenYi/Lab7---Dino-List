5abea39852947ef96bcac4b51d48235ea269c435
cindy <uenyihung@gmail.com>
2016-11-27 17:19:13 -0500

    asynchTask for db access works

--- app/src/main/java/com/example/a1031002/lab7/MainActivity.java
+++ app/src/main/java/com/example/a1031002/lab7/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.a1031002.lab7;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -13,12 +15,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ListView lv;
     private String[] dinoNames, dinoInfos;
     private int[] dinoPics, dinoIcon;
     private DinoDAO dinoDAO;
+    private DinoAdapter dinoAdapter;
+    private DataLoader dataLoader;
+    Cursor cursor;
+    int ctr, size;
     private String TAG = "MainActivity";
 
     @Override
@@ -27,11 +34,18 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Cursor cursor = dinoDAO.getDinos();
-        int ctr = 0;
-
         lv = (ListView) findViewById(R.id.dinoList);
+
+        dataLoader = new DataLoader();
+        dataLoader.execute();
+
+
+
+        /*
+        cursor = dinoDAO.getDinos();
+        ctr = 0;
+
+
 
         int size = cursor.getCount();
 
@@ -49,8 +63,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         cursor.close();
-
-        lv.setAdapter(new DinoAdapter(this, dinoNames, dinoPics, dinoInfos, dinoIcon));
+        dinoAdapter = new DinoAdapter(this, dinoNames, dinoPics, dinoInfos, dinoIcon);
+        lv.setAdapter(dinoAdapter);*/
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,4 +88,84 @@ public class MainActivity extends AppCompatActivity {
                 return false;
         }
     }
+
+    /**
+     * AsyncTask that will call in the background database actions.
+     */
+    public class DataLoader extends AsyncTask<Void, Integer, DinoAdapter> {
+        private final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        /**
+         * This probably "won't show up" as it is so short.
+         */
+        protected void onPreExecute() {
+            this.dialog.setTitle(getString(R.string.loading));
+            this.dialog.setMessage(getString(R.string.loadMsg));
+            this.dialog.show();
+        }
+
+        /**
+         * Gets the data from SQLite and load them into arrays
+         * to pass to the custom dino adapter. All done in the bkg.
+         *
+         * @param params
+         * @return
+         */
+        @Override
+        protected DinoAdapter doInBackground(Void... params) {
+            Cursor asyncCursor = dinoDAO.getDinos();
+
+            size = asyncCursor.getCount();
+
+            dinoNames = new String[size];
+            dinoInfos = new String[size];
+            dinoPics = new int[size];
+            dinoIcon = new int[size];
+
+            while (asyncCursor.moveToNext()) {
+                dinoNames[ctr] = asyncCursor.getString(asyncCursor.getColumnIndex(dinoDAO.COL_NAME));
+                dinoInfos[ctr] = asyncCursor.getString(asyncCursor.getColumnIndex(dinoDAO.COL_INFO));
+                dinoPics[ctr] = asyncCursor.getInt(asyncCursor.getColumnIndex(dinoDAO.COL_IMG_ID));
+                dinoIcon[ctr] = asyncCursor.getInt(asyncCursor.getColumnIndex(dinoDAO.COL_ICON_ID));
+                ctr++;
+
+                /**
+                 * This is only here so I can see what it actually does as my db is too small.
+                 */
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {
+
+                }
+
+                int percent = ctr * 100 / size;
+                publishProgress(percent);
+
+            }
+
+            asyncCursor.close();
+
+            return new DinoAdapter(MainActivity.this, dinoNames, dinoPics, dinoInfos, dinoIcon);
+        }
+
+        /**
+         * Every 2% of data added to the arrays, update the progress dialog.
+         *
+         * @param progress
+         */
+        protected void onProgressUpdate(Integer... progress) {
+            this.dialog.setMessage(getString(R.string.loadMsg) + "\t" + progress[0] + "%");
+        }
+
+        protected void onPostExecute(DinoAdapter asyncDA) {
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+
+            dinoAdapter = asyncDA;
+            dinoAdapter.notifyDataSetChanged();
+            lv.setAdapter(dinoAdapter);
+        }
+    }
 }
+
